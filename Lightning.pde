9f7f6ae587f4ab2f370fcb8170fa520320e1d317
unknown <shfeng1@697-334-Theoden.sf.k12.internal>
2014-09-05 10:12:21 -0700

    turning in Lightning Assignment

--- Lightning.pde
+++ Lightning.pde
@@ -1,13 +1,40 @@
+int lightX = 0;
+int lightY = 0;
+int lightX1 = 0;
+int lightY1 = 0;
+int opacity = 120;
 void setup()
 {
-  size(300,300);
+  size(500,500);
+  background(0);
+  strokeWeight(2.5);
+  frameRate(3);
 }
 void draw()
-{
-
+{	
+  //range of rgb of electric blue
+ while(lightX1 < 500)
+ {
+ 	stroke((int)(Math.random()*39)+44,(int)(Math.random()*14)+104,(int)(Math.random()*136)+120,opacity);
+    lightX1 = lightX + (int)(Math.random()*10);
+ 	lightY1 = lightY + (int)(Math.random()*10)-1;
+ 	line(lightX,lightY,lightX1,lightY1);
+ 	lightX = lightX1;
+ 	lightY = lightY1;
+ 	opacity = opacity - 1;
+ }
+ opacity = 120;
+ lightX = 0;
+ lightY = 0;
+ lightX1 = 0;
+ lightY1 = 0;
 }
-void mousePressed()
+void keyPressed()
 {
-
+	if(keyCode == ' ')
+	{
+		background(0);
+	}
 }
+
 
--- build-tmp/source/Lightning.java
+++ build-tmp/source/Lightning.java
@@ -14,20 +14,45 @@ import java.io.IOException;
 
 public class Lightning extends PApplet {
 
+int lightX = 0;
+int lightY = 0;
+int lightX1 = 0;
+int lightY1 = 0;
+int opacity = 120;
 public void setup()
 {
-  size(300,300);
-  noLoop();
+  size(500,500);
+  background(0);
+  strokeWeight(2.5f);
+  frameRate(3);
 }
 public void draw()
-{
-  int x = (int)(Math.random() * 300);
-  ellipse(x,150,10,10);
+{	
+  //range of rgb of electric blue
+ while(lightX1 < 500)
+ {
+ 	stroke((int)(Math.random()*39)+44,(int)(Math.random()*14)+104,(int)(Math.random()*136)+120,opacity);
+    lightX1 = lightX + (int)(Math.random()*10);
+ 	lightY1 = lightY + (int)(Math.random()*10)-1;
+ 	line(lightX,lightY,lightX1,lightY1);
+ 	lightX = lightX1;
+ 	lightY = lightY1;
+ 	opacity = opacity - 1;
+ }
+ opacity = 120;
+ lightX = 0;
+ lightY = 0;
+ lightX1 = 0;
+ lightY1 = 0;
 }
-public void mousePressed()
+public void keyPressed()
 {
-  redraw();
+	if(keyCode == ' ')
+	{
+		background(0);
+	}
 }
+
 
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "Lightning" };
